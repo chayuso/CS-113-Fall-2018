538295fc0c3d810d25baf2ec7d3f53f4c91ec226
Christopher Ayuso <chayuso14@gmail.com>
2018-10-13 16:11:48 -0700

    Front Tile Select Highlighter

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,6 +15,8 @@ public class PlayerMovement : MonoBehaviour {
     float addZ = 0;
     public string inputHorizontal = "Horizontal";
     public string inputVertical = "Vertical";
+    public string Direction = "N";
+    public GameObject frontTile;
     GameState GS;
     // Use this for initialization
     void Start () {
@@ -24,6 +26,13 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Movement();
+        InvalidTileCheck();
+        Direction = QuarterDirection();
+        CheckFrontTile();
+    }
+    void InvalidTileCheck()
+    {
+        //Checks and reverts movement if walking in a occupied tile
         if (GS.BlockedTiles.Contains(UpdateTilePosition()))
         {
             while (GS.BlockedTiles.Contains(UpdateTilePosition()))
@@ -39,6 +48,38 @@ public class PlayerMovement : MonoBehaviour {
             }
         }
     }
+
+    bool CheckFrontTile()
+    {
+        string frontS = "";
+        Vector3 tileFront = new Vector3(
+            findPosition(transform.position.x + DirectionNumbers(Direction)[0], offsetX, "x"),
+            findPosition(transform.position.z + DirectionNumbers(Direction)[1], offsetY, "y"),
+            findPosition(transform.position.y, offsetZ, "z"));
+        frontS = tileFront.x.ToString() + "x" + tileFront.y.ToString() + "x" + tileFront.z.ToString();
+
+        GameObject tempFrontTile = GameObject.Find(frontS);
+        if (frontTile)
+        {
+            if (frontTile != tempFrontTile)
+            {
+                frontTile.GetComponent<TileHighlight>().Dehighlight();
+            }
+        }
+        frontTile = tempFrontTile;
+        if (frontTile)
+        {
+            if (frontTile.tag != "HalfTile")
+            {
+                frontTile = null;
+                return false;
+            }
+            frontTile.GetComponent<TileHighlight>().Highlight();
+            return true;
+        }
+        return false;
+    }
+
     void Movement()
     {
         float moveHorizontal = Input.GetAxisRaw(inputHorizontal);
@@ -104,5 +145,98 @@ public class PlayerMovement : MonoBehaviour {
         }
 
         return return_tile_position;
+    }
+
+    float[] DirectionNumbers(string d)
+    {
+        float[] t = { 0, 0 };
+        if (d == "E")
+        {
+            t[0] = 1;//x
+            t[1] = 0;//y
+        }
+        else if (d == "W")
+        {
+            t[0] = -1;//x
+            t[1] = 0;//y
+        }
+        else if (d == "N")
+        {
+            t[0] = 0;//x
+            t[1] = 1;//y
+        }
+        else if (d == "S")
+        {
+            t[0] = 0;//x
+            t[1] = -1;//y
+        }
+        return t;
+    }
+
+    string QuarterDirection()
+    {
+        string D = "N";
+        if (transform.eulerAngles.y >= 0 && transform.eulerAngles.y <= 45f)
+        {
+            D = "N";
+        }
+        if (transform.eulerAngles.y >= 315f && transform.eulerAngles.y <= 360f)
+        {
+            D = "N";
+        }
+        else if (transform.eulerAngles.y > 45f && transform.eulerAngles.y < 135f)
+        {
+            D = "E";
+        }
+        else if (transform.eulerAngles.y >= 135f && transform.eulerAngles.y <= 225f)
+        {
+            D = "S";
+        }
+        else if (transform.eulerAngles.y > 225f && transform.eulerAngles.y < 315f)
+        {
+            D = "W";
+        }
+        return D;
+    }
+    string CompassDirection()
+    {
+        string compassDirection = "N";
+        if (transform.eulerAngles.y >= 0 && transform.eulerAngles.y <= 22.5f)
+        {
+            compassDirection = "N";
+        }
+        if (transform.eulerAngles.y >= 337.5f && transform.eulerAngles.y <= 360f)
+        {
+            compassDirection = "N";
+        }
+        else if (transform.eulerAngles.y >= 22.6f && transform.eulerAngles.y <= 67.4f)
+        {
+            compassDirection = "NE";
+        }
+        else if (transform.eulerAngles.y >= 67.5f && transform.eulerAngles.y <= 112.5f)
+        {
+            compassDirection = "E";
+        }
+        else if (transform.eulerAngles.y >= 112.6f && transform.eulerAngles.y <= 157.4f)
+        {
+            compassDirection = "SE";
+        }
+        else if (transform.eulerAngles.y >= 157.5f && transform.eulerAngles.y <= 202.5)
+        {
+            compassDirection = "S";
+        }
+        else if (transform.eulerAngles.y >= 202.6f && transform.eulerAngles.y <= 247.4)
+        {
+            compassDirection = "SW";
+        }
+        else if (transform.eulerAngles.y >= 247.5f && transform.eulerAngles.y <= 292.5)
+        {
+            compassDirection = "W";
+        }
+        else if (transform.eulerAngles.y >= 292.6f && transform.eulerAngles.y <= 337.4)
+        {
+            compassDirection = "NW";
+        }
+        return compassDirection;
     }
 }
---
+++ Assets/Scripts/TileHighlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileHighlight : MonoBehaviour {
+    public Material regularMat;
+    public Material HighlightMat;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Highlight()
+    {
+        GetComponent<Renderer>().material =HighlightMat;
+    }
+    public void Dehighlight()
+    {
+        GetComponent<Renderer>().material = regularMat;
+    }
+}
