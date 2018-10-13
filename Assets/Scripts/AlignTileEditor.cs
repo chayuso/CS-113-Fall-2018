90e20ef810daff20595f1378208641a41db8a7de
Christopher Ayuso <chayuso14@gmail.com>
2018-10-12 20:40:30 -0700

    Added AlignTile Script w/Brick Prefab

---
+++ Assets/Scripts/AlignTileEditor.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(AlignTile))]
+public class AlignTileEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        AlignTile myScript = (AlignTile)target;
+        if (GUILayout.Button("Align"))
+        {
+            foreach (AlignTile at in FindObjectsOfType<AlignTile>())
+            {
+                at.Align();
+            }
+        }
+    }
+}
+#endif
