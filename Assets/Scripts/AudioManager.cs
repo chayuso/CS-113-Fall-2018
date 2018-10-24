bf7d186ed958578ebb306a72510d099c60916ad2
Zxzzxz2 <supremegamer1@gmail.com>
2018-10-23 18:27:39 -0700

    Added AudioManager to play sounds
    Very basic AudioManager. Use AudioManager.manager.PlaySound(string name) to play a sound.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    
+    public static AudioManager manager;
+    public List<Song> sounds = new List<Song>();
+    private Dictionary<string, AudioSource> soundsDict = new Dictionary<string, AudioSource>();
+
+    void Start ()
+    {
+        manager = this;
+
+        for (int i = 0; i < sounds.Count; ++i)
+            soundsDict[sounds[i].name] = sounds[i].source;
+	}
+	
+	public void PlaySound(string name)
+    {
+        soundsDict[name].Play();
+    }
+
+    public void ChangeSoundPosition(string name, Vector3 newPosition)
+    {
+        soundsDict[name].gameObject.transform.position = newPosition;
+    }
+}
+
+[System.Serializable]
+public struct Song
+{
+    public string name;
+    public AudioSource source;
+}
+
+
