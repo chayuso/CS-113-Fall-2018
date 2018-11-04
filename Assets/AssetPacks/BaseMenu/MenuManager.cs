99475c11bf4842916005c436722137b836da6aac
Zxzzxz2 <supremegamer1@gmail.com>
2018-11-03 17:41:18 -0700

    Added MenuManager and Example Menu Scene
    Very Basic, just fades the logo in and out and links to the sample scene.

---
+++ Assets/AssetPacks/BaseMenu/MenuManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuManager : MonoBehaviour {
+
+    public float transitionTime = 1f;
+    public float durationTime = 1f;
+    public List<CanvasGroup> uiElements;
+
+	IEnumerator Start ()
+    {
+        FadeIn(uiElements[0]);
+        yield return new WaitForSeconds(durationTime);
+        FadeOut(uiElements[0]);
+        yield return new WaitForSeconds(durationTime/2);
+        FadeIn(uiElements[1]);
+    }
+
+    public void FadeIn(CanvasGroup uiElement)
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 1, transitionTime));
+    }
+
+    public void FadeOut(CanvasGroup uiElement)
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 0, transitionTime/2));
+    }
+
+    public IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float lerpTime = 1)
+    {
+        float _timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        while (true)
+        {
+            timeSinceStarted = Time.time - _timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp(start, end, percentageComplete);
+
+            cg.alpha = currentValue;
+
+            if (percentageComplete >= 1) break;
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+	private void loadScene(string newScene)
+	{
+		SceneManager.LoadScene(newScene);
+	}
+
+	private void quitGame()
+	{
+		Application.Quit();
+	}
+}
